0a26a6902d1b685ee642540c010af5e0b7cb4916
Santosh Nagaraj <san.n1597@gmail.com>
2025-09-29 19:54:02 +0200

    fix s3 public access

--- terraform-project/modules/s3/main.tf
+++ terraform-project/modules/s3/main.tf
@@ -10,6 +10,15 @@ resource "aws_s3_bucket" "this" {
   tags   = merge(var.tags, { Name = var.bucket_name })
 }
 
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 output "bucket_id" {
   value = aws_s3_bucket.this.id
 }
